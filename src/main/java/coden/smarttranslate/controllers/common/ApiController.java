7e6ed727bcb3b9f24f334de086e5f51c5b163529
Denys Chernyshov <den.blackshov@gmail.com>
2021-04-27 19:59:13 +0200

    Fix

--- src/main/java/coden/smarttranslate/controllers/common/ApiController.java
+++ src/main/java/coden/smarttranslate/controllers/common/ApiController.java
@@ -1,5 +1,6 @@
 package coden.smarttranslate.controllers.common;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ApiController {
 
-    @PostMapping(value="/ping", produces = "text/plain")
+    @GetMapping(value="/ping", produces = "text/plain")
     public String ping(){
         return "It works :)";
     }
