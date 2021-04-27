d65cfecc0a2162923f88b560779d1a96c21a0a20
Denys Chernyshov <den.blackshov@gmail.com>
2021-04-27 20:37:53 +0200

    Fix config

--- remote/run.sh
+++ remote/run.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+# cd telebots/volkbot/
+# sudo pm2 start handlers.py --interpreter=python3 --name=volkbot
+# cd
+# cd telebots/DemotivatorBot
+# sudo pm2 start main.py --interpreter=python3 --name=demotivatorbot
+#
+cd
+cd smart-translate
+sudo pm2 start config.json
